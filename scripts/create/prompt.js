0878a7f71af0332f907fcefeb210c12b4b126768
chensuiyi <chenbimo@gmail.com>
2022-05-15 03:12:34 +0800

    📦 增加create命令，调整任何目录都能使用yicode

---
+++ scripts/create/prompt.js
@@ -0,0 +1,131 @@
+// 模块导入
+import path from 'path';
+import { keyBy, merge } from 'lodash-es';
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import ora from 'ora';
+import AdmZip from 'adm-zip';
+import { fileURLToPath } from 'url';
+
+// yicode相关
+import { rootDir, tempDir, cliDir } from '../../yicode/paths.js';
+// import yicodePackage from path.resolve(yicodePaths.cliDir, 'yicode', 'helper', 'package.js');
+import { isEmptyDirectory, downloadProject, relativePath, fn_firname, fn_filename } from '../../yicode/utils.js';
+
+// TODO: 后期支持版本下载
+// 项目模板配置
+const projectTemplateConfig = [
+    {
+        name: '基础模板(vue2-base-webpack)',
+        value: 'vue2-base-webpack',
+        describe: '通用Vue单页应用开发',
+        filename: 'yicode-template-vue2-base-webpack.zip',
+        url: 'https://static.chensuiyi.com/download/yicode-template-vue2-base-webpack.zip'
+    }
+];
+const projectTemplateConfigByValue = keyBy(projectTemplateConfig, 'value');
+
+// 提示参数收集
+let promptParams = {
+    // 是否创建项目
+    isCreateProject: true,
+    // 是否覆盖当前目录
+    isRewriteDirectory: true,
+    // 项目模板类型
+    projectTemplateType: 'vue-base',
+    // 执行命令
+    executeCommand: 'dev'
+};
+
+/**
+ * 是否创建新项目
+ */
+async function prompt() {
+    /**
+     * 解析yicode脚手架专用文件
+     * 如果该文件不存在，则认定为不是yicode项目
+     * 不是yicode项目，则询问是否需要下载模板
+     */
+    const _isCreateProject = await inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'isCreateProject',
+            message: `当前目录不是合法的yicode项目，是否创建新项目？（默认：是）`,
+            default: promptParams.isCreateProject
+        }
+    ]);
+    merge(promptParams, _isCreateProject);
+
+    // 如果选择不创建新项目
+    if (promptParams.isCreateProject === false) {
+        process.exit(1);
+    } else {
+        await isRewriteDirectory();
+    }
+}
+
+/**
+ * 是否覆盖当前目录
+ */
+async function isRewriteDirectory() {
+    if (isEmptyDirectory(rootDir) === false) {
+        let _isRewriteDirectory = await inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'isRewriteDirectory',
+                message: `当前目录不为空，请确认是否覆盖当前目录下的文件？`,
+                default: promptParams.isRewriteDirectory
+            }
+        ]);
+        merge(promptParams, _isRewriteDirectory);
+    }
+    await projectTemplateType();
+}
+
+/**
+ * 选择创建的项目模板类型
+ */
+async function projectTemplateType() {
+    try {
+        const _projectTemplateType = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'projectTemplateType',
+                message: '请选择一个项目类型',
+                loop: false,
+                default: promptParams.projectTemplateType,
+                choices: projectTemplateConfig
+            }
+        ]);
+
+        merge(promptParams, _projectTemplateType);
+
+        let projectItem = projectTemplateConfigByValue[promptParams.projectTemplateType];
+
+        // 下载项目
+        await downloadProject(projectItem);
+
+        // 创建zip压缩实例
+        const zip = new AdmZip(path.resolve(tempDir, projectItem.filename));
+
+        /**
+         * 如果覆盖当前目录的话，则清空根目录
+         */
+        if (promptParams.isRewriteDirectory === true) {
+            fs.emptyDirSync(rootDir);
+            await zip.extractAllTo(rootDir, true);
+        } else {
+            await zip.extractAllTo(tempDir, true);
+            await fs.removeSync(path.join(tempDir, projectItem.filename));
+            await fs.copySync(tempDir, rootDir);
+        }
+
+        // 移除临时目录
+        fs.removeSync(tempDir);
+    } catch (err) {
+        chalk.bgRed('下载失败');
+    }
+}
+
+export { prompt };
--- scripts/prompt.js
+++ scripts/prompt.js
@@ -4,8 +4,6 @@ import { keyBy, merge } from 'lodash-es';
 import fs from 'fs-extra';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import ora from 'ora';
-import AdmZip from 'adm-zip';
 import { fileURLToPath } from 'url';
 
 // yicode相关
@@ -13,158 +11,56 @@ import { rootDir, tempDir, cliDir } from '../yicode/paths.js';
 // import yicodePackage from path.resolve(yicodePaths.cliDir, 'yicode', 'helper', 'package.js');
 import { isEmptyDirectory, downloadProject, relativePath, fn_firname, fn_filename } from '../yicode/utils.js';
 
-// TODO: 后期支持版本下载
-// 项目模板配置
-const projectTemplateConfig = [
-    {
-        name: '基础模板(vue2-base-webpack)',
-        value: 'vue2-base-webpack',
-        describe: '通用Vue单页应用开发',
-        filename: 'yicode-template-vue2-base-webpack.zip',
-        url: 'https://static.chensuiyi.com/download/yicode-template-vue2-base-webpack.zip'
-    }
-];
-const projectTemplateConfigByValue = keyBy(projectTemplateConfig, 'value');
-
 // 提示参数收集
 let promptParams = {
-    // 是否创建项目
-    isCreateProject: true,
-    // 是否覆盖当前目录
-    isRewriteDirectory: true,
-    // 项目模板类型
-    projectTemplateType: 'vue-base',
     // 执行命令
     executeCommand: 'dev'
 };
-/**
- * 是否创建新项目
- */
-async function isCreateProject() {
-    /**
-     * 解析yicode脚手架专用文件
-     * 如果该文件不存在，则认定为不是yicode项目
-     * 不是yicode项目，则询问是否需要下载模板
-     */
-
-    let yicodeConfigPath = path.resolve(rootDir, 'yicode.config.js');
-    if (fs.pathExistsSync(yicodeConfigPath) === false) {
-        const _isCreateProject = await inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'isCreateProject',
-                message: `当前目录不是合法的yicode项目，是否创建新项目？（默认：是）`,
-                default: promptParams.isCreateProject
-            }
-        ]);
-        merge(promptParams, _isCreateProject);
-
-        // 如果选择不创建新项目
-        if (promptParams.isCreateProject === false) {
-            process.exit(1);
-        } else {
-            await isRewriteDirectory();
-        }
-    } else {
-        await executeCommand();
-    }
-}
-
-/**
- * 是否覆盖当前目录
- */
-async function isRewriteDirectory() {
-    if (isEmptyDirectory(rootDir) === false) {
-        let _isRewriteDirectory = await inquirer.prompt([
-            {
-                type: 'confirm',
-                name: 'isRewriteDirectory',
-                message: `当前目录不为空，请确认是否覆盖当前目录下的文件？`,
-                default: promptParams.isRewriteDirectory
-            }
-        ]);
-        merge(promptParams, _isRewriteDirectory);
-    }
-    await projectTemplateType();
-}
-
-/**
- * 选择创建的项目模板类型
- */
-async function projectTemplateType() {
-    try {
-        const _projectTemplateType = await inquirer.prompt([
-            {
-                type: 'list',
-                name: 'projectTemplateType',
-                message: '请选择一个项目类型',
-                loop: false,
-                default: promptParams.projectTemplateType,
-                choices: projectTemplateConfig
-            }
-        ]);
-
-        merge(promptParams, _projectTemplateType);
-
-        // 清空当前目录
-        if (promptParams.isRewriteDirectory === true) {
-            fs.emptyDirSync(rootDir);
-        }
-
-        let projectItem = projectTemplateConfigByValue[promptParams.projectTemplateType];
-
-        // 下载项目
-        await downloadProject(projectItem);
-
-        // 创建zip压缩实例
-        const zip = new AdmZip(path.resolve(tempDir, projectItem.filename));
-
-        // 解压zip到当前目录
-        // TODO: 如果为不覆盖解压，则不会解压文件
-        await zip.extractAllTo(rootDir, promptParams.isRewriteDirectory);
-
-        // 移除临时目录
-        fs.removeSync(tempDir);
-
-        // 执行命令
-        // await executeCommand();
-    } catch (err) {
-        chalk.bgRed('下载失败');
-    }
-}
 
 /**
  * 选择要执行的命令
  */
 async function executeCommand() {
+    let yicodeConfigPath = path.resolve(rootDir, 'yicode.config.js');
+    let choices = [
+        {
+            name: 'npm' + chalk.cyanBright('  切换npm源地址'),
+            value: 'npm'
+        },
+        {
+            name: 'version' + chalk.cyanBright('  查看版本信息'),
+            value: 'version'
+        }
+    ];
+
+    // 如果是合法的yicode项目，则具有更多命令
+    if (fs.pathExistsSync(yicodeConfigPath) === true) {
+        choices.unshift({
+            name: 'new' + chalk.cyanBright('  创建（页面/组件/路由/指令/过滤器）文件'),
+            value: 'new'
+        });
+        choices.unshift({
+            name: 'build' + chalk.cyanBright(' 进行线上（测试/正式）环境编译打包'),
+            value: 'build'
+        });
+        choices.unshift({
+            name: 'dev' + chalk.cyanBright('  在本机环境进行开发调试'),
+            value: 'dev'
+        });
+    } else {
+        choices.unshift({
+            name: 'create' + chalk.cyanBright('  创建新项目'),
+            value: 'create'
+        });
+    }
+
     let _executeCommand = await inquirer.prompt([
         {
             type: 'list',
             name: 'executeCommand',
             message: '请选择一个命令',
             default: promptParams.executeCommand,
-            choices: [
-                {
-                    name: 'dev' + chalk.cyanBright('  在本机环境进行开发调试'),
-                    value: 'dev'
-                },
-                {
-                    name: 'build' + chalk.cyanBright(' 进行线上（测试/正式）环境编译打包'),
-                    value: 'build'
-                },
-                {
-                    name: 'new' + chalk.cyanBright('  创建（页面/组件/路由/指令/过滤器）文件'),
-                    value: 'new'
-                },
-                {
-                    name: 'npm' + chalk.cyanBright('  切换npm源地址'),
-                    value: 'npm'
-                },
-                {
-                    name: 'version' + chalk.cyanBright('  查看版本信息'),
-                    value: 'version'
-                }
-            ]
+            choices: choices
         }
     ]);
     merge(promptParams, _executeCommand);
@@ -174,4 +70,4 @@ async function executeCommand() {
     await prompt();
 }
 
-export default isCreateProject;
+export default executeCommand;
